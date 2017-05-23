f2c3e3002c480f9c72b7963991b7e8c39530cc73
Arpit Goyal <arpit.go4@gmail.com>
2017-05-23 18:45:28 +0530

    header and footer comps added

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import Header from './Header.component';
+import Footer from './Footer.component';
+
 export default class App extends React.Component {
 
 	render() {
 		return (
 			<div className="container-fluid">
-				
+				<div className="center-block">
+					<div className="row">
+						<div className="col-xs-6 col-xs-offset-3">
+							<Header />
+						</div>
+					</div>	
+					<div className="row">
+						<div className="col-xs-6 col-xs-offset-3">
+							<Footer />
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -4,10 +4,8 @@ export default class Header extends React.Component {
 
 	render() {
 		return (
-			<div className="center-block row">
-				<div className="col-xs-6 col-xs-offset-3">
-					
-				</div>
+			<div className="heading">
+				<p className="heading-text">Twitch Streamers</p>
 			</div>
 		);
 	}
