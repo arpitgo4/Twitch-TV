c573989287f2cfa70271542088abfa2dc7adbd64
Arpit Goyal <arpit.go4@gmail.com>
2017-05-23 18:14:01 +0530

    basic project structure and title set

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.component';
+import './index.html';
+
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+
+import './style.scss';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('react-app')
+);
