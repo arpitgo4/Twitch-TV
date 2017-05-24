a89e8d04358c8123325f658f417469755ec351d7
Arpit Goyal <arpit.go4@gmail.com>
2017-05-24 12:50:23 +0530

    sliders aligned with header comp

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -7,11 +7,17 @@ export default class Header extends React.Component {
 	render() {
 		return (
 			<div className="heading">
-				<p className="heading-text">Twitch Streamers</p>
-				<div className="heading-menu pull-right">
-					<Menu />
+				<div className="row">
+					<div className="col-xs-8 col-xs-offset-0">
+						<p className="heading-text">Twitch Streamers</p>
+					</div>
+					<div className="col-xs-4 col-xs-offset-0">
+						<div className="heading-menu pull-right">
+							<Menu />
+						</div>
+					</div>
 				</div>
-			</div>
+			</div>	
 		);
 	}
 
